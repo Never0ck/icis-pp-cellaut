3d778dc4f28ff78a5fe35d3bb6d2ebf219968993
Kamolov Amir <kamolov.amir2000@yandex.ru>
2022-10-22 23:46:10 +0300

    [!] Переход с WPF на MAUI

---
+++ icis-pp-cellaut/Cellaut.Presentation.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Cellaut.Presentation.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
